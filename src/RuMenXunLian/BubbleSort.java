17139013569448fd59b50e0e174889d8944cedbd
dadading <15305626179@189.cn>
2021-03-31 19:41:43 +0800

    JavaStudy20210331_01

--- src/RuMenXunLian/BubbleSort.java
+++ src/RuMenXunLian/BubbleSort.java
@@ -11,11 +11,17 @@ public class BubbleSort {
 		int[] arr1 = {5,4,3,2,1};
 		
 		for(int i=0;i<arr1.length-1;i++){
+			int flag = 0;
 			for(int m=0;m<arr1.length-1-i;m++){
 				if(arr1[m]>arr1[m+1]){
 					int tmp = arr1[m];
 					arr1[m] = arr1[m+1];
 					arr1[m+1] = tmp;
+					flag = 1;
+				}
+				//若本身就是有序数列,则只需外循环一次
+				if (flag==0){
+					break;
 				}
 			}
 		}
--- src/RuMenXunLian/Solution.java
+++ src/RuMenXunLian/Solution.java
@@ -12,12 +12,17 @@ public class Solution {
     public int[] MySort (int[] arr) {
         // write code here
         for(int i=0;i<arr.length-1;i++){
-        	for(int m=0;m<arr.length-i;m++){
+        	int flag = 0;
+        	for(int m=0;m<arr.length-1-i;m++){
         		if(arr[m]>arr[m+1]){
         			int tmp = arr[m];
         			arr[m] = arr[m+1];
         			arr[m+1] = tmp;
+        			flag = 1;
         		}
+        	}
+        	if(flag==0){
+        		return arr;
         	}
         }
         return arr;

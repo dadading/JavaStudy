eb2a9b9e7916a21a7df841532dc3201f54c8b0e0
dadading <15305626179@189.cn>
2021-04-26 17:19:31 +0800

    JavaStudy20210426

--- src/Day_20210426/Shopping.java
+++ src/Day_20210426/Shopping.java
@@ -1,6 +1,25 @@
+package Day_20210426;
+
 import java.util.Scanner;
 
-public class Test2 {
+/*
+ * KiKi非常喜欢网购，在一家店铺他看中了一件衣服，他了解到，如果今天是“双11”（11月11日）
+ * 则这件衣服打7折，“双12” （12月12日）则这件衣服打8折，如果有优惠券可以额外减50元
+ * （优惠券只能在双11或双12使用），求KiKi最终所花的钱数。
+ * 
+ * 
+ * 输入描述：
+ * 一行，四个数字，第一个数表示小明看中的衣服价格，第二和第三个整数分别表示当天的月份、
+ * 当天的日期、第四个整数表示是否有优惠券（有优惠券用1表示，无优惠券用0表示）。
+ * 
+ * 
+ * 输出描述：
+ * 一行，小明实际花的钱数（保留两位小数）。（提示：不要指望商家倒找你钱）
+ * 
+ * 
+ * */
+
+public class Shopping {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("请输入信息：");
